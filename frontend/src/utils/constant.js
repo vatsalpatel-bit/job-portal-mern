094cf61b0892484d1b32bc615f8ffef87d07f9f2
Vatsal patel <vatsalpatel13092006@gmail.com>
2026-06-26 13:05:06 +0530

    fix:base url

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,6 @@
-export const USER_API_END_PORT = "/api/v1/user";
-export const JOB_API_END_PORT = "/api/v1/job";
-export const APPLICATION_API_END_PORT="/api/v1/application";    
-export const COMPANY_API_END_PORT="/api/v1/company"
+const BASE_URL = import.meta.env.VITE_BACKEND_URL;
+
+export const USER_API_END_PORT = `${BASE_URL}/api/v1/user`;
+export const JOB_API_END_PORT = `${BASE_URL}/api/v1/job`;
+export const APPLICATION_API_END_PORT = `${BASE_URL}/api/v1/application`;
+export const COMPANY_API_END_PORT = `${BASE_URL}/api/v1/company`;
