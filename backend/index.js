6e71e0fb39464d20e1da5e10a7b3ad326da15302
Vatsal patel <vatsalpatel13092006@gmail.com>
2026-06-26 15:05:44 +0530

    add:trusty proxy

--- backend/index.js
+++ backend/index.js
@@ -24,6 +24,7 @@ import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
 
 const app = express();
+app.set("trust proxy", 1);
 
 // helmet
 app.use(helmet());
