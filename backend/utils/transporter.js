941b6da5a3906cb94e9ca5b0a70d0d16ad3a0c4a
Vatsal patel <vatsalpatel13092006@gmail.com>
2026-06-28 09:57:50 +0530

    fix:nodemailer (WIP)

--- backend/utils/transporter.js
+++ backend/utils/transporter.js
@@ -1,5 +1,13 @@
 import nodemailer from "nodemailer";
 
+console.log("EMAIL_USER:", process.env.EMAIL_USER);
+console.log(
+    "EMAIL_PASS:",
+    process.env.EMAIL_PASS ? "Exists" : "Missing"
+);
+
+await transporter.verify();
+console.log("SMTP Connected");
 export const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
@@ -7,3 +15,4 @@ export const transporter = nodemailer.createTransport({
         pass: process.env.EMAIL_PASS,
     },
 });
+console.log("After sendMail");
