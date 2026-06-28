1546871cd65f6282e7b89be45a810e08711a3833
Vatsal patel <vatsalpatel13092006@gmail.com>
2026-06-28 11:23:16 +0530

    fix:add smt in transpoter

--- backend/utils/transporter.js
+++ backend/utils/transporter.js
@@ -7,7 +7,9 @@ console.log(
 );
 
 export const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false, // true only for port 465
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
