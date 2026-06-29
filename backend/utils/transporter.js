b8a5de1a139eec07784d6774d5684a84e75bf22c
Vatsal patel <vatsalpatel13092006@gmail.com>
2026-06-29 12:49:09 +0530

    feat:add resend email

--- backend/utils/transporter.js
+++ backend/utils/transporter.js
@@ -1,18 +1,3 @@
-import nodemailer from "nodemailer";
+import { Resend } from "resend";
 
-console.log("EMAIL_USER:", process.env.EMAIL_USER);
-console.log(
-    "EMAIL_PASS:",
-    process.env.EMAIL_PASS ? "Exists" : "Missing"
-);
-
-export const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false, // true only for port 465
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-    },
-});
-console.log("After sendMail");
+export const resend = new Resend(process.env.RESEND_API_KEY);
